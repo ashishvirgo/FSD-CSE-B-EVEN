728272effc8f689332ef8922c04282a5764d479e
Ashish Bajpai <ashishvirgo12@gmail.com>
2026-02-16 15:23:29 +0530

    server code updated

--- server/server.js
+++ server/server.js
@@ -1,52 +1,47 @@
-import http from 'http';
-import os, { arch } from 'os';
-let body="";
-let data=[];
+import http from "http";
+import os from "os";
+const userdata=[];
 const server=http.createServer((req,res)=>{
-    const url=req.url;
-    
-    if(url=="/" && req.method=="GET"){
-        res.write("<h1>Home Page</h1>")
-    }
-    else if(url=="/about" && req.method=="GET"){
-        res.write("<h1>About Page1</h1>")
-    }
-    else if(url=="/contact"&& req.method=="GET"){
-        res.write("<h1>Contact Page</h1>")
-    }
-    else if(url=="/senddata"&& req.method=="POST"){
-        
-        req.on("data",(chunk)=>{
-            body=body+chunk;
-        })
-        req.on("end",()=>{
-            console.log(body," data recived ");
-            data.push(body);
-            res.end(body+" data recived");
-        })
-    }
-    else if(url=="/viewdata"&& req.method=="GET"){
-        res.setHeader("Content-Type","application/json")
-        res.end(JSON.stringify(data))
-    }
-    else if(url=="/system"&& req.method=="GET"){
-        const sysdata={
-            platform: os.platform(),
-            arch: os.arch(),
-            cpu: os.cpus().length,
-            totalRam:(os.totalmem()/1024**3).toFixed(2)+"GB",
-            freemem: (os.freemem()/1024**3).toFixed(2)+"GB"
-        }
-        res.setHeader("Content-Type","application/json")
-        res.end(JSON.stringify(sysdata))
-    }
-    else{
-        res.statusCode=404;
-        res.end("Error Page")
-    }
-    
-})
+const url=req.url;
+const method=req.method;
+if(url=="/" && method=="GET"){
+    res.end("Home Page")
+}
+else if(url=="/contact" && method=="GET"){
+    res.end("Contact Page")
+}
+else if(url=="/system" && method=="GET"){
+    const sysdata={
+        platform:os.platform(),
+        architecture: os.arch(),
+        cpu_length: os.cpus().length,
+        TotalMemory: (os.totalmem()/1024**3).toFixed(2)+"GB",
+        FreeMemory: (os.freemem()/1024**3).toFixed(2)+"GB"
 
-server.listen(4001,()=>{
-    console.log("Server is running on port 4001")
+    }
+    res.end(JSON.stringify(sysdata))
+}
+else if(url=="/senddata" && method=="POST"){
+    let body="";
+    req.on("data",(chunk)=>{
+      body=body+chunk;
+    })
+    req.on("end",()=>{
+        console.log(body,"Data send successfully")
+        res.statusCode=201;
+        userdata.push(body)
+        res.end(JSON.stringify(userdata))
+    })
+}
+else if(url=="/viewdata" && method=="GET"){
+    res.setHeader("Content-Type","application/json")
+    res.end(JSON.stringify(userdata));
+}
+else{
+    res.statusCode=404;
+    res.end("Error Page")
+}
+})
+server.listen(5001,()=>{
+    console.log(`Server is running on port 5001`)
 })
